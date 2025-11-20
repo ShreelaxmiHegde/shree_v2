f9831025c7d98f1e6c28c80a07f6aa9e561e594b
ShreelaxmiHegde <shreelaxmihegde9@gmail.com>
2025-11-20 15:33:36 +0530

    improve skills section

--- app/components/Skills.tsx
+++ app/components/Skills.tsx
@@ -1,19 +1,61 @@
 const skills = {
-  frontend: ["Javascript", "React.js", "Bootstrap"],
-  backend: ["Node.js", "Express.js"],
-  Database: ["MongoDB", "MySQL"]
+  Core: ["JavaScript", "React.js", "Node.js", "Express.js", "MongoDB"],
+  FamiliarWith: ["TypeScript", "Next.js", "TailwindCSS" ,"Bootstrap", "Material-UI"],
+  Tools: ["Git & Github", "Hoppscotch", "Render", "Vercel", "Cloudinary", "Docker"]
 };
 
 export default function Skills() {
   return (
-    <section id="skills" className="py-20 px-6 max-w-6xl mx-auto">
-      <h2 className="text-3xl font-bold">Skills</h2>
-      <div className="mt-6 flex flex-wrap gap-3">
-        {/* {skills.map(s => (
-          <span key={s} className="px-4 py-2 bg-gray-100 rounded-lg border text-sm">
-            {s}
-          </span>
-        ))} */}
+    <section className="section" id="skills">
+      <span className="text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-[#0379fc] to-[#22ffd7]">
+        Skills & Expertise
+      </span>
+
+      <div className="flex flex-wrap gap-4 mt-8">
+        {Object.entries(skills).map(([category, list]) => (
+          <div key={category} className="card w-xs max-w-md">
+            {category === "Core" ? (
+              <div>
+                <span className="text-xl font-semibold mb-3 text-transparent bg-clip-text bg-gradient-to-r from-[#0379fc] to-[#22ffd7]">
+                  <i className="fa-solid fa-code me-2"></i>
+                  {category}
+                </span>
+                <p className="text-gray-400 text-sm my-3">
+                  Technologies I use daily for development.
+                </p>
+              </div>
+            ) : category === "FamiliarWith" ? (
+              <div>
+                <span className="text-xl font-semibold mb-3 text-transparent bg-clip-text bg-gradient-to-r from-[#0379fc] to-[#22ffd7]">
+                  <i className="fa-solid fa-arrow-up-right-dots me-2"></i>
+                  Familiar With
+                </span>
+                <p className="text-gray-400 text-sm my-3">
+                  Technologies I also worked with.
+                </p>
+              </div>
+            ) : (
+              <div>
+                <span className="text-xl font-semibold mb-3 text-transparent bg-clip-text bg-gradient-to-r from-[#0379fc] to-[#22ffd7]">
+                  <i className="fa-solid fa-screwdriver-wrench me-2"></i>
+                  {category}
+                </span>
+                <p className="text-gray-400 text-sm my-3">
+                  Tools I use to get things done.
+                </p>
+              </div>
+            )}
+            <div className="flex flex-wrap gap-3 mt-2">
+              {list.map((skill) => (
+                <span key={skill}
+                  className="px-2 py-1 rounded-full bg-white/5 border border-white/10 text-sm text-[#2288ff] hover:bg-white/10 transition"
+                >
+                  {skill}
+                </span>
+              ))}
+            </div>
+          </div>
+        ))}
       </div>
     </section>
   );

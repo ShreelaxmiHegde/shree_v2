343ac67884891fbbd3f43945d3a530b2a16bf578
ShreelaxmiHegde <shreelaxmihegde9@gmail.com>
2025-12-21 18:59:21 +0530

    add logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,6 +41,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <link rel="icon" type="image/svg+xml" href="./public/logo.svg" />
         <link
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"

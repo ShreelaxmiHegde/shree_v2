8bbdb1471130d13f20d18af5390f01d0eb763608
ShreelaxmiHegde <shreelaxmihegde9@gmail.com>
2026-01-11 21:35:15 +0530

    update: add stdlib contribution

--- app/components/Experience.tsx
+++ app/components/Experience.tsx
@@ -4,6 +4,23 @@ export default function Experience() {
             <span className="section-name">Experience </span>
 
             <div className="relative border-l border-gray-700 pl-6 space-y-10 mt-8">
+                {/* Open Source #stdlib-js */}
+                <div className="relative">
+                    <div className="timeline-dot"></div>
+
+                    <h3 className="exp-heading">
+                        <a href="https://github.com/stdlib-js/stdlib/issues?q=is%3Apr+is%3Aopen+author%3AShreelaxmiHegde">
+                            Contributing to <code>#stdlib-js</code><i className="fa-solid fa-arrow-up-right-from-square ml-1"></i>
+                        </a>
+                    </h3>
+                    <p className="exp-time">Dec 2025 - Present</p>
+
+                    <ul className="exp-details">
+                        <li>Documentation improvements of <code>ndarray</code> instances.</li>
+                        <li>Fixed <code>Javascript</code> and <code>EditorConfig</code> lint errors.</li>
+                    </ul>
+                </div>
+
                 {/* Open Source */}
                 <div className="relative">
                     <div className="timeline-dot"></div>
@@ -23,13 +40,13 @@ export default function Experience() {
                     <div className="timeline-dot"></div>
 
                     <h3 className="exp-heading">Full-Stack Developer - Accommate (MERN App)</h3>
-                    <p className="exp-time">Aug 2025 - Present</p>
+                    <p className="exp-time">Aug 2025 - November 2025</p>
 
                     <ul className="exp-details">
                         <li>Built complete eCommerce platform with Auth & backend APIs.</li>
-                        <li>Implemented CRUD operations on Listings.</li>
+                        <li>Implemented CRUD operations on Listings and reviews.</li>
                         <li>Integrated Cloudinary for product media uploads.</li>
-                        <li>Deployed frontend & backend on Render.</li>
+                        <li>Deployed frontend on Vercel & backend on Render.</li>
                     </ul>
                 </div>
 
@@ -44,7 +61,6 @@ export default function Experience() {
                         <li>Built a productivity extension using JavaScript + Manifest V3.</li>
                         <li>The extension can hold upto 10 copy histories.</li>
                         <li>Make smoother workflow for retrieving the copies.</li>
-                        <li>Implemented DOM automation features.</li>
                         <li>Code fully available on GitHub (not published due to fees).</li>
                     </ul>
                 </div>
